fc240b9fa79baf156da68589e25eac1f6e2db3f5
Trevern G. LAMONTAGNE <trevernlamontagne10@gmail.com>
2025-11-05 20:00:22 -0400

    Added javascript functionality for subscribe button

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
-/* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', () => {
+    const message = document.querySelector(".message");
+    const emailIn = document.querySelector("#email");
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const email = emailIn.value.trim();
+
+        if (email){
+            message.textContent = `Thank you! Your email address ${email} has been added to our mailing list`;
+        } else {
+            message.textContent = "Please enter a valid email address";
+        }
+
+        emailIn.value = "";
+    })
+
+})
